f0fea9f5f20ff2245643de817398783b51b8dc29
Ana Clara <anaclaraa02pereira@gmail.com>
2025-04-06 00:36:05 -0300

    Mesclando alterações remotas com as dependências adicionadas

---
+++ src/test/java/com/anaclara/api_despesas_java_08/ApiDespesasJava08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anaclara.api_despesas_java_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDespesasJava08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
